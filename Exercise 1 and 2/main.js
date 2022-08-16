e845720e10706b5b9e2ea62832a58b551a9e797d
Carlos Pacha Pinto <86452932+LaFuriaPacha@users.noreply.github.com>
2022-08-15 19:29:13 -0500

    Add files via upload

---
+++ Exercise 1 and 2/main.js
@@ -0,0 +1,9 @@
+    //select the first h1 element
+    const headingOne= document.querySelector('h1');
+
+    //changes heading two variables  color
+    headingOne.style.color = "#134e4a";
+    //change the background color
+    function updateColors(){
+        headingOne.style.color = "#FF5733";
+    }
